16bd9e63d9f74719efe53ccd5e95007c187356ed
viswamprasad <viswamprasad@gmail.com>
2018-01-28 00:52:39 +0530

    Added Mock Server

--- src/test/java/com/testrest/setup/SetupEnv.java
+++ src/test/java/com/testrest/setup/SetupEnv.java
@@ -1,6 +1,8 @@
 package com.testrest.setup;
 
+import com.testrest.mockserver.MockServer;
 import io.restassured.RestAssured;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import java.io.File;
@@ -9,16 +11,32 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class SetupEnv {
+    public static boolean isMockServerOn = false;
+    public static Properties prop;
 
     @BeforeClass
     public static void setup() {
         try {
-            Properties prop = new Properties();
+            prop = new Properties();
             File testEnvFile = new File("src/test/resources/testenv.properties");
             prop.load(new FileInputStream(testEnvFile));
-            RestAssured.baseURI = prop.getProperty("basewsurl");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if ("1".equals(System.getProperty("mock"))) {
+            MockServer.start();
+            isMockServerOn = true;
+        }
+    }
+
+    @AfterClass
+    public static void close() {
+        if (isMockServerOn) {
+            MockServer.stop();
+        }
+    }
+
+    public static void setBaseURI(String url) {
+        RestAssured.baseURI = prop.getProperty(url.toLowerCase());
     }
 }
--- src/test/java/com/testrest/smoke/SmokeTestSuite.java
+++ src/test/java/com/testrest/smoke/SmokeTestSuite.java
@@ -1,18 +1,55 @@
 package com.testrest.smoke;
 
 import com.testrest.setup.SetupEnv;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.hasItems;
 
 import io.restassured.http.ContentType;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class SmokeTestSuite extends SetupEnv {
+    @Test
+    public void testMockServer() {
+        SetupEnv.setBaseURI("baseurl");
+        given()
+                .when().get("/test")
+                .then().statusCode(200);
+    }
 
     @Test
+    public void testForDriverSignup() {
+        SetupEnv.setBaseURI("baseurl");
+        given()
+                .when().body("{\n" +
+                "  \"user\": {\n" +
+                "    \"cc\": \"viswa\",\n" +
+                "    \"last_name\": \"prasad\",\n" +
+                "    \"mobile\": 9611909045,\n" +
+                "    \"driving_licence\": \"ADKPV19287\",\n" +
+                "    \"password\": \"viswa123\",\n" +
+                "    \"vin\": \"KA04MD484\",\n" +
+                "    \"email\": \"viswa.prasad@cognitiveclouds.com\"\n" +
+                "  }\n" +
+                "}").post("/users/driver_signup")
+                .then().statusCode(200);
+    }
+
+    @Test
+    @Category(com.testrest.smoke.SmokeTestSuite.class)
     public void testForStateName() {
         given()
                 .when().get("/state/get/USA/all")
-                .then().contentType(ContentType.JSON).body("RestResponse.result.name", hasItems("California"));
+                .then().contentType(ContentType.JSON).body("RestResponse.result.name", hasItems("Californiaaa"));
+    }
+
+    @Test
+    @Category(com.testrest.smoke.SmokeTestSuite.class)
+    public void testForLargerCity() {
+        given()
+                .when().get("/state/get/USA/all")
+                .then().contentType(ContentType.JSON).body("RestResponse.result.largest_city", hasItems("Los Angeles"));
+
     }
 }
